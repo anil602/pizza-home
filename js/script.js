eaeaa1661691eafae5f8a74d308ca049189154e9
anil602 <kashyap.anil602@gmail.com>
2018-05-23 18:52:57 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    var nav = $('#navbarMain');
+    var top = 200;
+    if ($(window).scrollTop() >= top) {
+
+        nav.addClass('fixed');
+
+    } else {
+        nav.removeClass('fixed');
+    }
+});
